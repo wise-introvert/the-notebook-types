698c6b5466e77cb1923f1127d466e7561162f854
Fardeen Panjwani <fardeen.panjwani@ucmouli.com>
2020-09-19 23:28:40 +0530

    fixed getRoute function

--- __tests__/utils/routes.ts
+++ __tests__/utils/routes.ts
@@ -15,12 +15,25 @@ describe("routes", () => {
       expect(route).toBe("/api/v1/user");
     });
 
-    it('returns "/custom-base/user/auth" when options object with base is provided along with "user" and "auth"', () => {
+    it('returns "/api/v1/user/auth" when called with "/user" and "/auth"', () => {
+      const sub: string = "/user";
+      const leaf: string = "/auth";
+      const route: string = getRoute(sub, leaf);
+      expect(route).toBe("/api/v1/user/auth");
+    });
+
+    it('returns "/api/v1/user" when called with just "/user"', () => {
+      const sub: string = "/user";
+      const route: string = getRoute(sub);
+      expect(route).toBe("/api/v1/user");
+    });
+
+    it('returns "/custom-base/user/auth" when options object with base is provided along with "/user" and "/auth"', () => {
       const options: any = {
         base: "/custom-base"
       };
-      const sub: string = "user";
-      const leaf: string = "auth";
+      const sub: string = "/user";
+      const leaf: string = "/auth";
       const route: string = getRoute(sub, leaf, options);
       expect(route).toBe("/custom-base/user/auth");
     });
--- src/utils/route.utils.ts
+++ src/utils/route.utils.ts
@@ -2,6 +2,10 @@ export interface GetRouteOptions {
   base?: string;
 }
 
+const replaceFirstSlash: Function = (path: string): string => {
+  return path.replace(/\/(.*)/g, "$1");
+};
+
 export const getRoute = (
   sub: string,
   leaf?: string,
@@ -10,11 +14,14 @@ export const getRoute = (
   if (!sub) {
     throw new Error("Invalid arguments");
   }
-  const subPath: string = `/${sub.replace(/\/$/g, "")}`;
-  const base: string = (options?.base || "/api/v1").replace(/\/$/g, "");
+  const subPath: string = `/${replaceFirstSlash(sub).replace(/\/$/g, "")}`;
+  const base: string = replaceFirstSlash(options?.base || "/api/v1").replace(
+    /\/$/g,
+    ""
+  );
   if (leaf) {
-    const leafPath: string = `/${leaf.replace(/\/$/g, "")}`;
-    return `${base}${subPath}${leafPath}`;
+    const leafPath: string = `/${replaceFirstSlash(leaf).replace(/\/$/g, "")}`;
+    return `/${base}${subPath}${leafPath}`;
   }
-  return `${base}${subPath}`;
+  return `/${base}${subPath}`;
 };
